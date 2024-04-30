4165080a7122c81d576984abd72bf89f847996ea
Vitalii Plechak <vitalii@ict.lv>
2024-04-29 21:51:54 -0600

    - Adjusted the `WEATHER_API_GEOCODING_URL` value

--- src/conf.ts
+++ src/conf.ts
@@ -1,5 +1,5 @@
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_GEOCODING_URL =
-  "http://api.openweathermap.org/geo/1.0";
+  "https://api.openweathermap.org/geo/1.0";
 export const WEATHER_API_KEY = import.meta.env?.VITE_WEATHER_API_KEY;
 export const REPO_LINK = "https://github.com/Vitalii-Plechak/Weather-App";
